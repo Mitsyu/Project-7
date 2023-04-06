94065bf9654cd277961b285aca4b5691797cec9f
Jila Phonprasith <jilaphonprasith@gmail.com>
2023-04-06 12:00:18 -0400

    part 2 add

--- Program.cs
+++ Program.cs
@@ -42,7 +42,7 @@ namespace Project_7
         private static void ListOfIntegers()
         {
             //Part 1
-            Console.WriteLine("List of integers program");
+            Console.WriteLine("- List of integers program -");
 
             List<int> numbers = new List<int>();
             Random rand = new Random();
@@ -198,17 +198,141 @@ namespace Project_7
 
                     default:
                         Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Invalid choice. Choose from (1-10)");
+                        Console.WriteLine("Invalid option. Please enter a number from (1-10)");
                         break;
                 }
             }
         }
         private static void ListOfStrings()
         {
+            //Part 2 
+            Console.WriteLine();
+            Console.WriteLine("- List of strings program -");
+            Console.WriteLine();
 
+            string choice = "";
+            List<string> vegetables = new List<string> { "CARROT", "BEET", "CELERY", "RADISH", "CABBAGE" };
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine();
 
+            PrintVegetables(vegetables);
+
+            while (true)
+            {
+                Console.ResetColor();
+                Console.WriteLine();
+                Console.WriteLine("---------------------------------------------------------");
+                Console.WriteLine();
+                Console.WriteLine("Please select an option (0-6):");
+                Console.WriteLine("1. Remove a vegetable by index");
+                Console.WriteLine("2. Remove a vegetable by value");
+                Console.WriteLine("3. Search for a vegetable");
+                Console.WriteLine("4. Add a vegetable");
+                Console.WriteLine("5. Sort list");
+                Console.WriteLine("6. Clear the list");
+                Console.WriteLine("7. Quit");
+                Console.WriteLine();
+                Console.Write("Option: ");
+                choice = Console.ReadLine().ToLower().Trim();
+                //int option = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                Console.WriteLine("---------------------------------------------------------");
+                Console.WriteLine();
+
+                switch (choice)
+                {
+                    case "7":
+                        Environment.Exit(0);
+                        break;
+
+                    case "1":
+                        Console.Write("Please enter the index of the vegetable you wish to remove: ");
+                        int indexToRemove = Convert.ToInt32(Console.ReadLine());
+                        if (indexToRemove >= 0 && indexToRemove < vegetables.Count)
+                        {
+                            vegetables.RemoveAt(indexToRemove);
+                            Console.WriteLine("Vegetable removed.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid index.");
+                        }
+                        break;
+
+                    case "2":
+                        Console.Write("Please enter the name of the vegetable you wish to remove: ");
+                        string valueToRemove = Console.ReadLine().ToUpper();
+                        if (vegetables.Remove(valueToRemove))
+                        {
+                            Console.WriteLine($"Vegetable ({valueToRemove}) removed.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Vegetable not found.");
+                        }
+                        break;
+
+                    case "3":
+                        Console.Write("Please enter the name of the vegetable you wish to search for: ");
+                        string valueToSearch = Console.ReadLine().ToUpper();
+                        int index = vegetables.IndexOf(valueToSearch);
+                        if (index >= 0)
+                        {
+                            Console.WriteLine($"Vegetable found at index {index}.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Vegetable not found.");
+                        }
+                        break;
+
+                    case "4":
+                        Console.Write("Please enter the name of the vegetable you wish to add: ");
+                        string valueToAdd = Console.ReadLine().ToUpper();
+                        if (vegetables.Contains(valueToAdd))
+                        {
+                            Console.WriteLine("Vegetable already in list.");
+                        }
+                        else
+                        {
+                            vegetables.Add(valueToAdd);
+                            Console.WriteLine("Vegetable added.");
+                        }
+                        break;
+
+                    case "5":
+                        vegetables.Sort();
+                        Console.WriteLine("List sorted.");
+                        break;
+
+                    case "6":
+                        vegetables.Clear();
+                        Console.WriteLine("List cleared.");
+                        break;
+
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Invalid option. Please enter a number from (1-7)");
+                        break;
+                }
+
+                PrintVegetables(vegetables);
+            }
+        }
+
+        static void PrintVegetables(List<string> vegetables)
+        {
+            Console.ResetColor();
+            Console.WriteLine("Vegetables");
+            for (int i = 0; i < vegetables.Count; i++)
+            {
+                Console.WriteLine($"{i + 1} - {vegetables[i]}");
+            }
         }
     }
+
 }
+   
+
 
             
